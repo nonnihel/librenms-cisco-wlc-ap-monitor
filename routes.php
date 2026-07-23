b596d2159384c8ab877866753001b90ccc79ffc8
nonnihel <nonnih@gmail.com>
2026-07-23 15:43:16 +0000

    Add access point detail route

--- routes.php
+++ routes.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AccessPointController::class, 'index'])->name('index');
 Route::get('/widget', [AccessPointController::class, 'widget'])->name('widget');
+Route::get('/access-points/{id}', [AccessPointController::class, 'show'])
+    ->whereNumber('id')
+    ->name('show');
 Route::post('/access-points/{id}/action', [AccessPointController::class, 'action'])
     ->whereNumber('id')
     ->name('action');
